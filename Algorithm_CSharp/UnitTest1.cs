b94750b9e844a8e5926f9fed225f6d8f8adeb646
kwen won lee <highfence@naver.com>
2023-01-14 15:59:12 +0900

    Solve problem 1061

--- Algorithm_CSharp/UnitTest1.cs
+++ Algorithm_CSharp/UnitTest1.cs
@@ -1,3 +1,5 @@
+using System.Text;
+
 namespace Algorithm_CSharp
 {
     public class Tests
@@ -24,6 +26,55 @@ namespace Algorithm_CSharp
         public bool CanArrange(int[] arr, int k)
         {
             return false;
+        }
+
+        private static readonly object[] TestSource_1061 =
+        {
+            new object[] { "parker", "morris", "parser", "makkek" },
+            new object[] { "hello", "world", "hold", "hdld" },
+            new object[] { "leetcode", "programs", "sourcecode", "aauaaaaada" },
+        };
+
+        [TestCaseSource(nameof(TestSource_1061))]
+        public void Test_1061(string s1, string s2, string baseStr, string expectedResult)
+        {
+            var result = SmallestEquivalentString(s1, s2, baseStr);
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
+
+        public string SmallestEquivalentString(string s1, string s2, string baseStr)
+        {
+            Dictionary<char, char> translator = new();
+
+            for (var i = 0; i < s1.Length; i++)
+            {
+                var c1 = s1[i];
+                var c2 = s2[i];
+
+                var lexicographcallySmallerOne = c1 < c2 ? c1 : c2;
+
+                if (translator.ContainsKey(c1) == false)
+                {
+                    translator.Add(c1, lexicographcallySmallerOne);
+                }
+                if (translator.ContainsKey(c2) == false)
+                {
+                    translator.Add(c2, lexicographcallySmallerOne);
+                }
+                
+                var translatedC1 = translator[c1];
+                var translatedC2 = translator[c2];
+
+                var bestOne = new[] { translatedC1, translatedC2, lexicographcallySmallerOne }.Min();
+
+                var charactersInGroup = translator.Where(entry => entry.Value == translatedC1 || entry.Value == translatedC2 || entry.Value == lexicographcallySmallerOne).Select(entry => entry.Key);
+                foreach (var character in charactersInGroup)
+                {
+                    translator[character] = bestOne;
+                }
+            }
+
+            return new string(baseStr.Select(c => translator.ContainsKey(c) ? translator[c] : c).ToArray());
         }
     }
 }
