b159d1d167a07757460db1096b92ff1a7ec2635d
근원 이 <Highfence@naver.com>
2023-01-16 10:56:25 +0900

    Add Some Test Util

---
+++ Algorithm_CSharp/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm_CSharp
+{
+	internal class Problems_2023_Week3
+	{
+		class Test_57
+		{
+			public static readonly object[] TestSource = new object[]
+			{
+				new object[] { new int[][] { new int[] { 1, 3 }, new int[] { 6, 9 } }, new int[] { 2,5 }, new int[][] { new int[] { 1, 5 }, new int[] { 6, 9 } },
+				new object[] { new int[][] { new int[] { 1, 2 }, new int[]{3,5},new int[]{6,7},new int[]{8, 10}, new int[]{12, 16} }, new int[] {4, 8 }, new int[][] { new int[]{1,2}, new int[]{3,10}, new int[]{12,16} } } }
+			};
+
+			[TestCaseSource(nameof(TestSource))]
+			public void Test(int[][] intervals, int[] newInterval, int[][] expectedInterval)
+			{
+				
+			}
+
+		}
+
+	}
+}
---
+++ Algorithm_CSharp/Utilities.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Algorithm_CSharp
+{
+	internal class Utilities
+	{
+
+	}
+
+	public class TestArgument
+	{
+		public string Value { get; set; }
+		public TestArgument(string value) 
+		{
+			this.Value = value;
+		}
+
+		public static implicit operator TestArgument(string value)
+		{
+			return new TestArgument(value);
+		}
+
+		public static implicit operator string(TestArgument argument)
+		{
+			return argument.Value;
+		}
+
+		// "string value [[1,2],[3,10]] translate with int[][] type"
+		public int[][] ToJaggedIntArray()
+		{
+			var value = Value.Replace(" ", "");
+
+			var inDimensionString = value.Substring(1, value.Length - 2);
+			var firstDimensionLength = Regex.Matches(inDimensionString, "]").Count;
+			var result = new int[firstDimensionLength][];
+
+			var innerStartIndex = 0;
+			var innerEndIndex = 0;
+
+			for (int innerIndex = 0; innerIndex < Regex.Matches(inDimensionString, "]").Count; ++innerIndex)
+			{
+				innerStartIndex = inDimensionString.IndexOf('[', innerEndIndex);
+				innerEndIndex = inDimensionString.IndexOf(']', innerStartIndex);
+
+				var intArray = inDimensionString.Substring(innerStartIndex, innerEndIndex - innerStartIndex).Replace("[", "").Replace("]", "").Split(',').Select(int.Parse).ToArray();
+				result[innerIndex] = intArray;
+			}
+
+			return result; 
+		}
+	}
+
+	public class Test_Utilities
+	{
+		public readonly static object[] Source_TestArgument_StringToIntJaggedArray = new object[] 
+		{
+			new object[]{ "[[1,2],[3,10]]", new int[][] { new int[] { 1, 2 }, new int[] { 3, 10 } } }
+		};
+
+		[TestCaseSource(nameof(Source_TestArgument_StringToIntJaggedArray))]
+		public void Test_TestArgument_StringToIntJaggedArray(string inputValue, int[][] expected)
+		{
+			var argument = new TestArgument(inputValue);
+			Assert.That(expected, Is.EqualTo(argument.ToJaggedIntArray()));
+		}
+	}
+}
